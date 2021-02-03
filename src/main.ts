3e0b7fcad7e72ca2538ef8d8bc747470507d3dce
Renan <renan@prontocombustiveis.com.br>
2021-02-03 14:59:19 -0300

    configuração básica

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './modules/app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
+async function bootstrap() {
+  const port = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
+
+  Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
+}
+bootstrap();
