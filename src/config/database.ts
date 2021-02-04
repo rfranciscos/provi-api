222e4469efee38136f8135d450730fedcd10b9e1
Renan <renan@prontocombustiveis.com.br>
2021-02-04 01:58:56 -0300

    feat(typeorm): adicionando configs de conexão com o banco

---
+++ src/config/database.ts
@@ -0,0 +1,16 @@
+import { ConnectionOptions } from 'typeorm';
+
+export const databaseConfig: ConnectionOptions = {
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['entities/*.ts'],
+  migrationsTableName: 'custom_migration_table',
+  migrations: ['migrations/*.ts'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/app.module';
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
+import { databaseConfig } from '@config';
 
 async function bootstrap() {
   const port = process.env.PORT;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule.forRoot(databaseConfig));
   app.use(helmet());
   app.enableCors();
   app.use(
