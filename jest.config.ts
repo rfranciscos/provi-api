63929c7330f139c548c76533575b42fdaa97064d
Renan <renan@prontocombustiveis.com.br>
2021-02-04 18:17:09 -0300

    feat(jest): adicionando configução de testes

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@controller': '<rootDir>/src/controllers',
+    '@entities': '<rootDir>/src/entities',
+    '@modules': '<rootDir>/src/modules',
+    '@repositories': '<rootDir>/src/repositories',
+    '@services': '<rootDir>/src/services',
+    '@config': '<rootDir>/src/config',
+  },
+};
