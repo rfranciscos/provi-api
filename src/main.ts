64341c52c4e0306a18b9195d21fbf93c4913e31f
Renan <renan@prontocombustiveis.com.br>
2021-02-04 17:03:21 -0300

    feat(swagger): adicionando setup básico para documentação da API

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,20 @@ import { AppModule } from './modules/app.module';
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
 import { databaseConfig } from '@config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule.forRoot(databaseConfig));
+  const config = new DocumentBuilder()
+    .setTitle('Provi API')
+    .setDescription('The provi API')
+    .setVersion('1.0')
+    .addTag('provi')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(helmet());
   app.enableCors();
   app.use(
